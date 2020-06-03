460818c594898fd71f11d40c91361786f9b8244b
sena-code <sena.carbone@gmail.com>
2020-06-03 11:22:49 -0300

    Final

--- Divisao.cs
+++
@@ -1,9 +0,0 @@
-namespace Calculadora
-{
-    public class Divisao : CalculadoraCientifica
-    {
-        public float Dividir(){
-            return number1 / number2;
-        }
-    }
-}
--- Subtracao.cs
+++
@@ -1,9 +0,0 @@
-namespace Calculadora
-{
-    public class Subtracao : CalculadoraCientifica
-    {
-        public float Subitrair(){
-            return resultado = number1-number2;
-        } 
-    }
-}
